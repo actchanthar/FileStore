59877ad9f1c2c97ecf52bef58e3f13426d7ee40b
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-02-12 10:36:22 +0630

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -31,4 +31,22 @@ async def channel_post(client: Client, message: Message):
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+        await post_message.edit_reply_markup(reply_markup
+
+@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
+async def new_post(client: Client, message: Message):
+
+    if DISABLE_CHANNEL_BUTTON:
+        return
+
+    converted_id = message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔽 Download နှိပ်ပါ", url=f'{link}')]])
+    try:
+        await message.edit_reply_markup(reply_markup)
+    except Exception as e:
+        print(e)
+        pass
+        
