18f1b22f53621e1b74da9bfe23594ea30274d370
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-02-15 14:41:03 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>Bot was deploy by @actanibot again </blockquote></b>")
         except: pass
 
     async def stop(self, *args):
