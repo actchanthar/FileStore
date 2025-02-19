e436183b8d2494096ad25a9d98ac5b89484e2772
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-02-19 19:13:40 +0630

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ TIME = int(os.environ.get("TIME", "60"))
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "0"))
+FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1001610001670"))
 #put 0 to disable
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))#put 0 to disable
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))#put 0 to disable
