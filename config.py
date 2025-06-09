20ba6ef5bf92a7db3bf07b44e77a79426f3c82c9
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-06-09 16:19:34 +0630

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5062124930")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://act23:act23@cluster0.x1lrbcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "actit")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
