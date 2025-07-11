dade778c349e8a09143b9f04bcf77e5d22071041
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-07-11 15:37:56 +0630

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,7 +145,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/addlist/OS47OfNjVkhiNDg9")],
+                [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/addlist/6Nyr71Q5TTg2MTg1")],
                 [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data="about"),
                     InlineKeyboardButton('ʜᴇʟᴘ •', callback_data="help")
