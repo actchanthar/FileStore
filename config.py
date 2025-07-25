c456a7c86a93748e96e73d3928bab4ab16bdf069
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-07-25 13:57:46 +0630

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ from os import environ, getenv
 import logging
 from logging.handlers import RotatingFileHandler
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "Sorry Bro you gonna fk")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7828266427:AAH_FDIpvLHkquubr9wXenNYlluVuRevYsY")
 APP_ID = int(os.environ.get("APP_ID", "23034561"))
 API_HASH = os.environ.get("API_HASH", "30b5ce6ea2bbbbc3fe5368bbce4d2db8")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002798673047"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002772334487"))
 OWNER = os.environ.get("OWNER", "When_the_night_falls_my_soul_se")
 OWNER_ID = int(os.environ.get("OWNER_ID", "5062124930"))
 
 PORT = os.environ.get("PORT", "8009")
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Black")
+DB_NAME = os.environ.get("DATABASE_NAME", "nyi")
 
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/stranger77777777777")
